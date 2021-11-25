cfee4eb871f88b5e90c7f3f1108b9f92eec04891
lijianjian <18301162@bjtu.edu.cn>
2021-11-25 18:05:10 +0800

    首次提交

---
+++ src/test/java/com/nowcoder/community/MapperTest.java
@@ -0,0 +1,78 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+import java.util.List;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+//@ComponentScan(basePackages = "com.nowcoder.community")
+//@MapperScan("com.nowcoder.community.dao")
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(1);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("liubei@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("2.jpg");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "2-new.jpg");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "hello");
+        System.out.println(rows);
+
+    }
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(0, 0, 10);
+        for (DiscussPost post : list){
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(1);
+        System.out.println(rows);
+    }
+}
