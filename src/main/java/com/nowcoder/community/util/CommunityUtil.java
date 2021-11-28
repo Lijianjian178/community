597fe09418603183d14f460d51e69f2f2ce2b7e6
lijianjian <18301162@bjtu.edu.cn>
2021-11-28 11:41:53 +0800

    发布帖子

--- src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -1,8 +1,11 @@
 package com.nowcoder.community.util;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class CommunityUtil {
@@ -22,5 +25,30 @@ public class CommunityUtil {
         return DigestUtils.md5DigestAsHex(key.getBytes());
     }
 
+    public static String getJSONString(int code, String message, Map<String, Object> map){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", code);
+        jsonObject.put("message", message);
+        if (map != null){
+            for (String key : map.keySet()){
+                jsonObject.put(key, map.get(key));
+            }
+        }
+        return jsonObject.toJSONString();
+    }
 
+    public static String getJSONString(int code, String message) {
+        return getJSONString(code, message, null);
+    }
+
+    public static String getJSONString(int code) {
+        return getJSONString(code, null, null);
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "zhangsan");
+        map.put("age", 25);
+        System.out.println(getJSONString(0, "ok", map));
+    }
 }
